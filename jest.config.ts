f3e8404fe790ba9d7f35e001e011e76860a01273
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2021-12-09 11:16:14 -0600

    chore: add test coverage with junit reporting

--- jest.config.ts
+++ jest.config.ts
@@ -3,4 +3,15 @@ import { Config } from '@jest/types'
 export default {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    reporters: ['default', 'jest-junit'],
+    collectCoverage: true,
+    collectCoverageFrom: ['src/**/*.ts'],
+    coverageThreshold: {
+        global: {
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100,
+        },
+    },
 } as Config.InitialOptions
